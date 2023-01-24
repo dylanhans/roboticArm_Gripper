ece2e6220f529f88e4714f60ef3bb368199774f6
Dylan <80360273+dylanhans@users.noreply.github.com>
2023-01-24 10:41:51 -0500

    Update Arduino_Draft.ino

--- Arduino/Arduino_Draft.ino
+++ Arduino/Arduino_Draft.ino
@@ -3,48 +3,63 @@
 //Include libraries here
 #include <Servo.h>
 
-...
-
 //Define servo motors and variables
 #define servo1 0
-    
-...
+
+Servo finger1;
+Servo finger2;
+Servo finger3;
+
 int response_time = ?;
-int sensor1_data = A1;
-int sensor2_data = A2;
+const int finger1_data = A1;
+const int finger2_data = A2;
+const int finger3_data = 10;
+//Store servo position for default
+int pos = 0;
+
+
+//Below start functional code and smoothing motion
 ...
-
-
-
+    
+void Setup(){
+finger1.attach(finger1_data);
+finger2.attach(finger2_data);
+finger3.attach(finger3_data);
+    
 if (Serial.available() > 0) { 
     state = Serial.read(); // Reads the data from the serial port
     Serial.print(state); // Prints out the value sent
     //Motor functionality code block
 }
-
-delay (5);
-
-//Below start functional code and smoothing motion
-...
-    
-// Finger Movement 1 - Vertical
-    finger1.attach(servoPin1);
-    finger1.write(pos1);
-    
-// Finger Movement 2 - Vertical
-    finger2.attach(servoPin2);
-    finger2.write(pos2);
-
-
-// Finger Movement 3 - Horizontal
-    finger3.attach(servoPin3);
-    finger3.write(pos3);
-
-void loop{
-    //reads
-    
-    //set positions
-    
-
 }
+
+
+//Smooth motion code...
+
+void smooth{}
+
+//Make different positions to assign to fingers for seperate states.
+//Figure out wire movement to assign servo motion
+void loop{
+    // Finger Movement 1 - Vertical
+    // ex. 0 to 180
+  for(pos = 0; pos <= 180; pos += 1){
+    finger1.write(pos);
+    delay(15);
+  }
+    // Finger Movement 2 - Vertical
+  // ex. 180 degrees to 0 degrees
+  for(pos = 180; pos>=0; pos-=1){
+    finger2.write(pos);
+    delay(15);
+  }
+    // Finger Movement 3 - Horizontal
+   // ex. 60 degrees to 0 degrees
+  for(pos = 60; pos>=0; pos-=1){
+    finger3.write(pos);
+    delay(15);
+  }
+}
+    
+//Introduce button functionality and classes corresponding to situations
    
