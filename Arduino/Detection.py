45369a8cbca16674e6e269d290019c9b0a9db820
Dylan <80360273+dylanhans@users.noreply.github.com>
2023-01-24 20:22:04 -0500

    Update Detection.py

--- Arduino/Detection.py
+++ Arduino/Detection.py
@@ -1,1 +1,30 @@
-
+/* This file is to identify human real-time motion 
+   and ensure safety precautions are in place*/
+  
+  //Open cv library import
+  import cv2
+  from cvzone.HumanDetectionModule import HumanDetector
+  from cvzone.SerialModule import SerialObject
+  
+  cap = cv2.VideoCapture(1)
+  detector = HumanDetector()
+  arduino = SerialObject()
+  
+  //While statement (bboxs is bounding boxes)
+  
+  while True:
+    success, img = cap.read()
+    img, bboxs = detector.findHumans(img)
+    
+    //Integration can be linked to serial, led.
+    if bboxs:
+      arduino.sendData([1]) //turns led on
+    // link led to safety function for default state
+  
+    cv2.imshow("Human Detected", img)
+    cv2.waitKey(1)
+    
+  // Integrate functionality with arduino or ros, where successful detection will
+  // retract gripper to default state and output message
+  ...
+  
